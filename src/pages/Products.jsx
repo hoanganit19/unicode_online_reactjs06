b2be56eb12b4b8ff1b2328d01ecad3336a4010a5
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-07-18 12:54:19 +0700

    React Router DOM 09

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react";
 import { navigateForbidden } from "../ultis/forbidden";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Products() {
   const [posts, setPosts] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [keyword, setKeyword] = useState("");
   const navigate = useNavigate();
-  const getPosts = async () => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const handleChangeInputSearch = (e) => {
+    setKeyword(e.target.value);
+    setSearchParams({ query: e.target.value });
+  };
+  const getPosts = async (keyword = null) => {
+    let params = "";
+    if (keyword) {
+      params = "?q=" + keyword;
+    }
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts${params}`
+    );
     if (!response.ok) {
       return navigate("/404");
     }
@@ -17,11 +29,33 @@ export default function Products() {
   };
   useEffect(() => {
     document.title = "Danh sách sản phẩm";
-    getPosts();
+    const query = searchParams.get("query");
+    if (query) {
+      setKeyword(query);
+    } else {
+      getPosts();
+    }
   }, []);
+
+  useEffect(() => {
+    if (keyword) {
+      getPosts(keyword);
+    } else {
+      getPosts();
+    }
+  }, [keyword]);
+
   return (
     <div>
       <h1>Products</h1>
+      <div className="mb-3">
+        <input
+          type="search"
+          className="form-control"
+          placeholder="Từ khóa tìm kiếm..."
+          onChange={handleChangeInputSearch}
+        />
+      </div>
       {isLoading ? (
         <h3>Loading...</h3>
       ) : (
